106e8f6b8019004c523931ca8977c3c04d988cd9
Rajat2110 <rajatnaik2110@gmail.com>
2024-04-06 17:57:52 +0530

    Revert "First commit"
    This reverts commit b12935103d808f46d6c7555c44ee494363faf264.

--- quizApp/src/main/java/com/rajat/quizApp/dao/QuizDao.java
+++
@@ -1,7 +0,0 @@
-package com.rajat.quizApp.dao;
-
-import com.rajat.quizApp.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizDao extends JpaRepository<Quiz, Integer> {
-}
